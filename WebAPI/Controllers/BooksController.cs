5d6518a0aec6ec826149d5e3d4b228cdc01ce944
KaanUzun <kaanuzun.ku@outlook.com>
2020-04-18 22:41:11 +0200

    Add project files.

---
+++ WebAPI/Controllers/BooksController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Books")]
+    public class BooksController : Controller
+    {
+        private IBookService _bookService;
+        private BooksController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetList()
+        {
+            return Ok(_bookService.GetAll());
+        }
+    }
+}
